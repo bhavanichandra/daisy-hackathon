6159ca58412c6c74642ee89639a84f007d731892
bhavanichandra <bhavanichandra9@gmail.com>
2022-08-03 04:11:27 +0530

    Modified logic as per daisi requirement

--- aws/s3_driver.py
+++ aws/s3_driver.py
@@ -1,19 +1,46 @@
 import boto3
-from boto3.resources.base import ServiceResource
 from aws import AWSCredentials
+import os
+from uuid import uuid4
+from typing import Any, Dict
+
+from custom_types import AWSUploadMetadata
 
 
 class AWSClient:
-
     def __init__(self, credentials: AWSCredentials) -> None:
         """
         Initializes the AWSClient with AWS Credentials.
 
         :param credentials: AWS Credentials type
         """
-        self.session = boto3.client(credentials.service, aws_access_key_id=credentials.access_key,
-                                    aws_secret_access_key=credentials.access_secret, region_name=credentials.region)
+        self.session = boto3.client(
+            credentials.service,
+            aws_access_key_id=credentials.access_key,
+            aws_secret_access_key=credentials.access_secret,
+            region_name=credentials.region,
+        )
 
-    
-    
-    
+    def upload_file(
+        self, bucket_name: str, file: bytes, metadata: AWSUploadMetadata
+    ) -> Dict[str, Any]:
+        """
+        Upload File API is used to upload any file to the specified s3 bucket and the provided credentials
+
+        :param bucket_name: Name of the S3 Bucket where file to be stored
+        :param file: File in bytes
+        :param metadata: File Upload metadata such as Content Type, Content Length and File Name
+        """
+        name, extension = os.path.splitext(metadata.file_name)
+        key = f"{name}_{str(uuid4())}{extension}"
+        print(f"saving with key: {key} ")
+        put_object_response = self.session.put_object(
+            Key=key,
+            Bucket=bucket_name,
+            Body=file,
+            Metadata={
+                "Content-Type": metadata.content_type,
+                "Content-Length": str(metadata.content_length),
+            },
+        )
+        return put_object_response
--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-from curses import meta
-from io import TextIOWrapper
 from aws import AWSCredentials, s3_driver
-import os
-from dotenv import load_dotenv
-from uuid import uuid4
 
-import custom_types
-
-load_dotenv()
+from custom_types import AWSUploadMetadata
 
 
-def upload_file(file: bytes, bucket_name: str, credentials: AWSCredentials, metadata: custom_types.AWSUploadMetadata):
+def upload_file(
+    file: bytes,
+    bucket_name: str,
+    access_key: str,
+    access_secret: str,
+    region: str,
+    file_name: str,
+    content_type: str,
+    content_length: int,
+):
     """
     Upload File API is used to upload any file to the specified s3 bucket and the provided credentials
 
     :param file: File in bytes
     :param bucket_name: Name of the S3 Bucket where file to be stored
-    :param credentials: AWS Credentials
-    :param metadata: File Upload metadata such as Content Type, Content Length and File Name
+    :param access_key: AWS Access Key Id
+    :param access_secret: AWS Access Secret
+    :param region: AWS S3 Bucket Region
+    :param file_name: Name of the file
+    :param content_type: Content Type of the file
+    :param content_length: Size of the files
 
+    :returns: Returns a PutObjectResponse as Dictionary
     """
-    client = s3_driver.AWSClient(credentials)
-    name, extension = os.path.splitext(metadata.file_name)
-    key = f"{name}_{str(uuid4())}{extension}"
-    print(f"saving with key: {key} ")
-    put_object_response = client.session.put_object(
-        Key=key,
-        Bucket=bucket_name,
-        Body=file,
-        Metadata={
-            'Content-Type': metadata.content_type,
-            'Content-Length': str(metadata.content_length)
-        }
+    credentials = AWSCredentials(
+        service="s3", access_key=access_key, access_secret=access_secret, region=region
     )
-    return put_object_response
-
-
-
+    metadata = AWSUploadMetadata(
+        content_length=content_length, content_type=content_type, file_name=file_name
+    )
+    client = s3_driver.AWSClient(credentials)
+    return client.upload_file(bucket_name, file, metadata)
